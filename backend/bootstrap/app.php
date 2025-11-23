a07a9d303aded556a4a969ac5c99d34dc03b8028
Nixon Cuenca <ncuenca84@gmail.com>
2025-11-22 22:30:36 -0500

    Configurar Sanctum en bootstrap/app.php

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -12,7 +12,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+
+        // Sanctum necesario para tokens API
+        $middleware->api(prepend: [
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+        ]);
+
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
