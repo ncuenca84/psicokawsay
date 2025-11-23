8d3a6ba3b3daf86a4142a2cd5c10f349db4abafd
Nixon Cuenca <ncuenca84@gmail.com>
2025-11-23 11:15:14 -0500

    Actulizar User.php

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -4,12 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory;
 
     protected $fillable = [
         'name',
@@ -20,6 +19,5 @@ class User extends Authenticatable
 
     protected $hidden = [
         'password',
-        'remember_token',
     ];
 }
