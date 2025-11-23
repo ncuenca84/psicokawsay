7e3e5f0d956722d7033bb9808ef692a49ed5c17d
Nixon Cuenca <ncuenca84@gmail.com>
2025-11-22 22:25:56 -0500

    User model actualizado para Sanctum

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -2,30 +2,29 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * Campos que se pueden guardar masivamente
-     */
     protected $fillable = [
         'name',
         'email',
         'password',
-        'role',  // campo nuevo para roles
+        'role',
     ];
 
-    /**
-     * Campos ocultos cuando se devuelve JSON
-     */
     protected $hidden = [
         'password',
         'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
     ];
 }
